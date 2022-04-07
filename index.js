ab23a5b25fd0cbc27eec0b3f24ec30e272379087
patrice mocombe <patricemocombe@gmail.com>
2022-04-07 10:53:21 -0400

    commit tree 653419d019813b06d430caf58166c89c6f84b883

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(streetNumber) {
+    if (streetNumber >= 43)    
+    return streetNumber - hq;
+
+    else if (streetNumber <= 41)
+    return hq - streetNumber;
+};
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
+
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) *264;
+};
+distanceFromHqInFeet(43);
+distanceFromHqInFeet(50);
+distanceFromHqInFeet(34);
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination )
+    return (start - destination) *264;
+
+    else if (destination > start)
+    return (destination - start) * 264;
+};
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400)
+    return (distanceTravelledInFeet(start, destination) * 0);
+        
+    else if (distanceTravelledInFeet(start, destination) > 2500)
+    return `cannot travel that far`;
+        if (distanceTravelledInFeet(start, destination) > 2000)
+        return 25;
+            if(distanceTravelledInFeet(start, destination) > 400)
+            return ((distanceTravelledInFeet(start, destination) - 400) * .02);
+        
+};
+
+calculatesFarePrice(43, 44);
+calculatesFarePrice(34, 32);
+calculatesFarePrice (50, 58);
+calculatesFarePrice(34, 24);
